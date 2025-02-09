7f100d049f2fe5cca8643398bcfd98cf70fd3fc6
Retro <74841742+retropubg@users.noreply.github.com>
2025-02-09 13:17:20 -0400

    Update loader.py

--- loader.py
+++ loader.py
@@ -1,6 +1,6 @@
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-BOT_TOKEN = '7851924299:AAEj70SJUOzeS5w-GHUpUiK-GiPV1CHRaSE'
+BOT_TOKEN = '7020048572:AAG5bV9yhIk4DVw3ynUo-j9GHS743f9xVyA'
 bot = Bot(token=BOT_TOKEN, parse_mode=types.ParseMode.HTML)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
