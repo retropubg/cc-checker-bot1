e63f54810a16f11238b145aa7f593812c303ecaa
Retro <74841742+retropubg@users.noreply.github.com>
2025-02-11 20:33:19 -0400

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,3 +1,13 @@
-from .user import dp
+from .bin_info import register_handlers as register_bin_info
+from .mchk import register_handlers as register_mchk
+from .admin import register_handlers as register_admin
+from .sk import register_handlers as register_sk
+from .ssh import register_handlers as register_ssh
+from .callback import register_handlers as register_callback
+from .pp import register_handlers as register_pp
+from .scrape import register_handlers as register_scrape
 
-__all__ = ['dp']
+__all__ = [
+    'register_bin_info', 'register_mchk', 'register_admin', 'register_sk', 'register_ssh', 
+    'register_callback', 'register_pp', 'register_scrape'
+]
