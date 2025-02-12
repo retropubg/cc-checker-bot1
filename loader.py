0d04084a0f636581763ed3a9b244d8b2b1062668
Retro <74841742+retropubg@users.noreply.github.com>
2025-02-11 22:05:18 -0400

    Update loader.py

--- loader.py
+++ loader.py
@@ -2,11 +2,11 @@ from aiogram import Bot, Dispatcher
 from aiogram.types import ParseMode
 from aiogram.fsm.storage.memory import MemoryStorage
 
-BOT_TOKEN = '8048311747:AAGyGx8dCxU3zsDsct5Hd6T6Ign5G6gVq6Y'
+BOT_TOKEN = "8048311747:AAGyGx8dCxU3zsDsct5Hd6T6Ign5G6gVq6Y"
 
 bot = Bot(token=BOT_TOKEN, parse_mode=ParseMode.HTML)
 storage = MemoryStorage()
-dp = Dispatcher(storage=storage)  # En Aiogram 3, no se pasa bot aquí.
+dp = Dispatcher(storage=storage)  # En Aiogram 3, NO se pasa bot aquí.
 
 # Asociar el bot manualmente al dispatcher
 dp["bot"] = bot
